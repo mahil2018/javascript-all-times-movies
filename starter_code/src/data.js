3540284f8f751a9302a9114fc77e1f1799dc0c01
Maria <Maria@MBPMH003.local>
2018-11-06 18:05:55 -0500

    initial commit

--- starter_code/src/data.js
+++ starter_code/src/data.js
@@ -2000,3 +2000,56 @@ var movies = [
     rate: '8.0'
   }
 ];
+// Turn duration of the movies from hours to minutes 
+
+
+let newMovies = movies.map(function(elem){
+  let durationSplit = elem.duration.split([' ']);
+  let newRate = (parseInt(durationSplit[0],10)*60)+
+   (parseInt(durationSplit[1],10));
+  return elem.duration=newRate;
+});
+
+console.log(newMovies);
+console.log(movies);
+
+//average rates
+
+  let sumRate = movies.rate.map.reduce((base,elem){
+      sumRate=(base + elem);
+  return sumRate = (sumRate/movies.length).toFixed(2);
+	});
+console.log(sumRate);
+console.log(movies);
+
+//============
+// Get the average of all rates with 2 decimals 
+
+let rates = movies.map(function(elem) {
+  return Number(elem.rate);
+}).reduce(function(base,elem){
+  return base + elem;
+});
+let rateAverages = (rates/movies.length).toFixed(2);
+console.log(rateAverages);
+
+// Get the average of Drama Movies
+
+let avdrama = movies.filter(function(elem){
+  return elem.genre.includes('Drama');
+        
+});
+console.log (avdrama);
+console.log(avdrama.length/movies.length);
+
+Order by time duration, in growing order
+let newMoviesdursort = movies.sort(function(a,b){
+  return a.duration-b.duration;
+});
+console.log(newMoviesdursort);
+
+// How many movies did STEVEN SPIELBERG
+let spielberg = movies.filter(function(elem){
+  return elem.director.includes('Steven Spielberg');
+});
+console.log(spielberg);
--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -1,20 +1,37 @@
 /* eslint no-restricted-globals: 'off' */
 // Turn duration of the movies from hours to minutes 
-
+let newMovies = movies.map(function(elem){
+    let durationSplit = elem.duration.split([' ']);
+    let newRate = (parseInt(durationSplit[0],10)*60)+
+     (parseInt(durationSplit[1],10));
+    return elem.duration=newRate;
+  });
+  
+  console.log(newMovies);
+  console.log(movies);
 
 // Get the average of all rates with 2 decimals 
-
+let rates = movies.map(function(elem) {
+    return Number(elem.rate);
+  }).reduce(function(base,elem){
+    return base + elem;
+  });
+  let rateAverages = (rates/movies.length).toFixed(2);
+  console.log(rateAverages);
 
 // Get the average of Drama Movies
 
 
-// Order by time duration, in growing order
+let avdrama = movies.filter(function(elem){
+    return elem.genre.includes('Drama');
+          
+  });
+  console.log (avdrama);
+  console.log(avdrama.length/movies.length);
+  
 
 
-// How many movies did STEVEN SPIELBERG
-
-
-// Order by title and print the first 20 titles
+  
 
 
 // Best yearly rate average
